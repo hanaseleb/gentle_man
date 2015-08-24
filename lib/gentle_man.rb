0d429ca603cc248ae167fd3abaa85656457a0e1f
AkihiroSutoh <sutoh@naturally-curly-hair.com>
2015-08-24 21:31:36 +0900

    initial commit!

---
+++ lib/gentle_man.rb
@@ -0,0 +1,16 @@
+require "gentle_man/version"
+
+module Doala
+  def face
+    '(⌒(´・△・`)⌒)'
+  end
+
+  def business_friend
+    'つば九郎'
+  end
+
+module GentleMan
+  def hello
+    'hello'
+  end
+end
---
+++ lib/gentle_man/version.rb
@@ -0,0 +1,3 @@
+module GentleMan
+  VERSION = "0.1.0"
+end
